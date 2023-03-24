2edad2586177b1333d2eb2d5cd5edac95bb021e8
mdnumanahmed <mnumansyl@gmail.com>
2023-03-24 16:33:45 +0600

    Project initialized setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <h1>Welcome to Bongo Travelers Group</h1>
+    
+    </div>
+  )
+}
+
+export default App
