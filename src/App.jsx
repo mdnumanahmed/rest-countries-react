84252372fdc8dc0e4fea604a643e5b937a8ae1c8
mdnumanahmed <mnumansyl@gmail.com>
2023-03-24 16:41:25 +0600

    Data loaded from api

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,16 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  const [countries, setCountries] = useState([])
+
+  useEffect(()=>{
+    const loadCountries = async() => {
+      const res = await fetch('https://restcountries.com/v3.1/all')
+      const data = await res.json()
+      console.log(data);
+    }
+    loadCountries()
+  },[])
 
   return (
     <div className="App">
